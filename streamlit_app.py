5b3a2600033e9a505c11d2dfd45ebd6860969982
cchiaowang <89234212+cchiaowang@users.noreply.github.com>
2023-03-03 16:25:45 +0900

    Update streamlit_app.py
    added breakfast menu and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 st.title('New Restaurant')
 
 st.header ('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avcado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
