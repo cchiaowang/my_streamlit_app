1e48ff3695aa9d04a32fd27485b5e81a5c5124ae
cchiaowang <89234212+cchiaowang@users.noreply.github.com>
2023-03-03 16:57:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.header ('Breakfast Menu')
 st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avcado Toast')
+st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -15,7 +15,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include.
-st.multiselect('Pick some fruits:', list(my_fruit_list.index))
+st.multiselect('Pick some fruits:', list(my_fruit_list.index), ["Avocado", "Strawberries"])
 
 # Display the table on the page.
 st.dataframe(my_fruit_list)
